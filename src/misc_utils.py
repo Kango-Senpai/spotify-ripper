61a74c0168623524f618a2071b7f9762e3a08bd0
Kango-Senpai <hejalowe@outlook.com>
2023-06-30 14:50:18 -0500

    Initial commit.

---
+++ src/misc_utils.py
@@ -0,0 +1,13 @@
+import os
+import platform
+
+clear_cmd = ""
+
+def clear_screen():
+    os.system(clear_cmd)
+
+if platform.system() != "Windows":
+    clear_cmd = "clear"
+else:
+    clear_cmd = "cls"
+    
