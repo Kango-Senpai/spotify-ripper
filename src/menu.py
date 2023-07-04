a97271b37b67d140d08e73b150413d73f46f5773
Kango-Senpai <hejalowe@outlook.com>
2023-07-04 02:14:01 -0500

    Small tweak.

--- src/menu.py
+++ src/menu.py
@@ -1,5 +1,5 @@
 class Menu_Item:
-    def __init__(self, description, func_ptr):
+    def __init__(self, description, func_ptr, target_array):
         self.desc = description
         self.function = func_ptr
-        menu_items.append(self)
+        target_array.append(self)
