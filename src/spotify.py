638f7af7c0d537798b86bc202216bc3a76aa7b7b
Kango-Senpai <hejalowe@outlook.com>
2023-07-05 02:28:38 -0500

    Added function to collect playlist ids/urls.

--- src/spotify.py
+++ src/spotify.py
@@ -1,8 +1,32 @@
 import requests
+
+def spotify_main(access_token):
+    playlist_ids = []
+    print("Enter a spotify playlist or url to download.")
+    print("Type '#' when you're done.")
+    while True:
+        user_input = input("Playlist id or url: ")
+        if user_input.strip() == '#':
+            break
+        if user_input.strip() == '':
+            continue
+        print(user_input)
+        print("https" in user_input)
+        if "https" in user_input:
+            user_input = parse_url(user_input)
+        playlist_ids.append(user_input)
+    for playlist_id in playlist_ids:
+        print(get_playlist_items(playlist_id, access_token))
+    input()
+    
+
+
 def parse_url(playlist_url):
+    if "https" not in playlist_url:
+        return playlist_url
     #https://open.spotify.com/playlist/6464WxV1gWImrnYQfPnEqO?si=94efac4d66ab445f&pt=124c35ba0096c6f46a9105d2acde533c
     playlist_id = ""
-    landmark = "playlist/"
+    landmark = "/playlist/"
     index = playlist_url.index(landmark)
     index += len(landmark)
     
@@ -28,9 +52,9 @@ def get_playlist_items(playlist_id, access_token):
     
     # while index_offset <= total_items:
     for index_offset in range(0, total_items, batch_size):
-        print(f"Total items: {total_items}")
-        print(f"Index offset: {index_offset}")
-        print(f"Batch size: {batch_size}")
+        # print(f"Total items: {total_items}")
+        # print(f"Index offset: {index_offset}")
+        # print(f"Batch size: {batch_size}")
         api_request = requests.get(f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks?limit={batch_size}&offset={index_offset}",headers=request_header)
         if not api_request.ok:
             print(f"Failed to request playlist data. ({api_request.status_code}) Check your playlist id.")
@@ -42,4 +66,3 @@ def get_playlist_items(playlist_id, access_token):
         index_offset += batch_size
     input("Done. Press ENTER to continue...")
     return song_data
-
