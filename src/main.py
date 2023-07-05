314ee9d9b279b70a6ae86f73044c3ee425432649
Kango-Senpai <hejalowe@outlook.com>
2023-07-05 01:58:46 -0500

    Added function to handle menu arrays. This replaces main loop in main.py.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
 import config
-from misc_utils import clear_screen
 from spotify import get_playlist_items
+from spotify import parse_url
 from menu import Menu_Item
+from menu import activate_menu
 access_token = ""
 menu_items = []
 
-
-
 def test_spotify():
     global access_token
-    items = get_playlist_items("4HlJ2xsrkRpi3jYI1v60FD",access_token)
+    items = get_playlist_items("3AIdkgRsykCUVZeoMc7Pac",access_token)
+
+def parse_test():
+    print(parse_url("https://open.spotify.com/playlist/6464WxV1gWImrnYQfPnEqO?si=94efac4d66ab445f&pt=124c35ba0096c6f46a9105d2acde533c"))
+    input()
 
 def init(): #Wrap main in menu class
     global access_token
@@ -22,30 +25,10 @@ def init(): #Wrap main in menu class
     input("Press ENTER to continue...")
     return access_token
 
-def main():
-    access_token = init()
-    print(access_token)
-    while True:
-        clear_screen()
-        for i in range(0,len(menu_items)):
-            print(f"{i}. {menu_items[i].desc}")
-        user_input = input("Selection: ")
-        try:
-            user_input = int(user_input)
-        except:
-            print("Selection must be a number!")
-            input("Press ENTER to continue")
-            continue
-        try:
-            menu_items[user_input].function()
-        except:
-            print("Invalid selection!")
-            input("Press ENTER to continue")
-            continue
-
-
-Menu_Item("Main Menu",None)
-Menu_Item("Test spotify", test_spotify)
-Menu_Item("Exit", exit)
+Menu_Item("Main Menu",None, menu_items)
+Menu_Item("Test spotify", test_spotify, menu_items)
+Menu_Item("Exit", exit , menu_items)#Figure out a cleaner exit method.
+Menu_Item("Test parse",parse_test, menu_items)
 if __name__ == "__main__":
-    main()
+    access_token = init()
+    activate_menu(menu_items)
--- src/menu.py
+++ src/menu.py
@@ -1,5 +1,25 @@
+from misc_utils import clear_screen
 class Menu_Item:
     def __init__(self, description, func_ptr, target_array):
         self.desc = description
         self.function = func_ptr
         target_array.append(self)
+        
+def activate_menu(menu_array):
+    while True:
+        clear_screen()
+        for i in range(0,len(menu_array)):
+            print(f"{i}. {menu_array[i].desc}")
+        user_input = input("Selection: ")
+        try:
+            user_input = int(user_input)
+        except:
+            print("Selection must be a number!")
+            input("Press ENTER to continue")
+            continue
+        if user_input >= 1 and user_input <= len(menu_array):
+            menu_array[user_input].function()
+        else:
+            print("Invalid selection!")
+            input("Press ENTER to continue")
+            continue
